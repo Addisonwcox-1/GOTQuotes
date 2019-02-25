00ea597b34a1a04623d3f262c78dd96fc23054d6
Cox, Addison W <Addison.W.Cox-1@ou.edu>
2019-02-25 11:43:22 -0600

    GOT Quote JSON Practice

---
+++ JSON/GOT/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿//Addie Cox
+
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GOT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //https://got-quotes.herokuapp.com/quotes
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = client.GetAsync(@"https://got-quotes.herokuapp.com/quotes").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+
+                    Quote q = JsonConvert.DeserializeObject<Quote>(content);
+
+                    gotQuote.txt = new TextBlock();
+
+                    txt.Inlines.Add(new Italic(new Run("italic")));
+                    txt.Inlines.Add(new Bold(new Run("bold")));
+
+                    Content = txt;
+
+
+
+                }
+            }
+
+        }
+    }
+
+    public FormatTheText()
+    {
+
+        Title = "Format the text";
+    }
+}
